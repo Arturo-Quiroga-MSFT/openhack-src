7a6abe09ab6b0a1f4437a2678964f913374a600c
Arturo Quiroga <arturoqu@hotmail.ca>
2021-07-30 15:13:27 -0400

    back to poi 8080

---
+++ install-nginx.sh
@@ -0,0 +1,3 @@
+# from here https://github.com/kubernetes/ingress-nginx/blob/main/docs/deploy/index.md#azure
+#
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.48.1/deploy/static/provider/cloud/deploy.yaml
